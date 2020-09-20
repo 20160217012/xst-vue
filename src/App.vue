2f29a7f4c2a42113b9705e3bb07aae8a41d26ad9
quan <3526381910@qq.com>
2020-09-20 10:16:06 +0800

    薪算通人事部前端

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <transition name="fade">
+    <!-- router-view是整体内容的容器 -->
+    <router-view></router-view>        
+  </transition>
+</template>
+
+<script>
+  export default {
+  }
+</script>
